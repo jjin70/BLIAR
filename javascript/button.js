afe7b4115a04479aa9606541e4a792525ea422c2
jjin70 <jjin0434@naver.com>
2023-05-30 21:56:07 +0900

    230530완성

--- javascript/button.js
+++ javascript/button.js
@@ -1,11 +1,19 @@
 function deleteFriend() {
-    alert('친구를 삭제하겠습니까?');
+    confirm('친구를 삭제하시겠습니까?');
 }
 
+function sns() {
+    // let win=window.open(); // 빈 윈도우 열기
+    // win.location='https://naver.com'; // 네이버 페이지 로드
+    // window.open('https://naver.com')
+    // window.location.href='https://naver.com';
+    window.open('https://www.instagram.com/');
+}
 
+function github() {
+    window.open('http://github.com/');
+}
 
-// function load() {
-//     // let win=window.open(); // 빈 윈도우 열기
-//     // win.location='https://naver.com'; // 네이버 페이지 로드
-//     window.open('https://naver.com')
-// }
+function mail() {
+    window.open('https://mail.google.com/');
+}
--- javascript/logout.js
+++
@@ -1,6 +0,0 @@
-function logout() {
-    logout = confirm("계속하겠습니까?");
-    if (logout == true) {
-        <a href="myProfile_logout.html" target="_self"></a>
-    }
-}
